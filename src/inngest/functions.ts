9e3d62d0fc06899c11a591e814343db946435523
Prithiviraj <prithiviraj.m@target.com>
2025-08-01 11:31:36 +0530

    feat: add E2B sandbox integration and update functions for sandbox management

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,16 @@
 import { openai , createAgent } from '@inngest/agent-kit'
 import { inngest } from "./client";
+import { Sandbox } from '@e2b/code-interpreter';
+import { getSandbox } from './utils';
 
 export const helloworld = inngest.createFunction(
     { id: "hello-world"},
     { event: "test/hello.world" },
-    async ({ event }) => {
+    async ({ event, step }) => {
+        const sandboxId = await step.run('get-sandbox-id', async () => {
+            const sandbox = await Sandbox.create('vibe-nextjs-test-asd-2');
+            return sandbox.sandboxId;
+        })
         const summarizer = createAgent({
             name: "summarizer",
             system: "You are an expert summarizer. You summarize in 2 words",
@@ -14,8 +20,13 @@ export const helloworld = inngest.createFunction(
         const { output } = await summarizer.run(
             'Summarize the following text in 2 words: ' + event.data.value,
         )
-        console.log("Summarized output:", output);
-        return { output };
+
+        const sandboxUrl = await step.run('get-sandbox-url', async () => { 
+            const sandbox = await getSandbox(sandboxId);
+            const host =  sandbox.getHost(3000);
+            return `https://${host}`
+        })
+        return { output, sandboxUrl };
     }
 );
 
